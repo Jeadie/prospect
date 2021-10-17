087ff2a336fbb77d7aba967ab32bb0ca49c13f02
Jack Eadie <jeadi@amazon.com>
2021-10-17 15:17:19 +1000

    initial commit with reuter commodities

--- reuters.go
+++ reuters.go
@@ -18,23 +18,21 @@ type ResourceReference struct {
 }
 
 func main() {
-	reuters := "https://www.reuters.com/markets/commodities"
-	response, err := http.Get(reuters)
+	result := make([]ResourceReference, 0)
+	response, err := http.Get("https://www.reuters.com/markets/commodities")
 	if err != nil {
 		return
 	}
 
-	// Close response object once finished.
+	// response body is streamed on demand. Must close connection at end.
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
-
+			fmt.Println("an error occurred when closing http GET")
 		}
 	}(response.Body)
 
-	doc, err := goquery.NewDocumentFromReader(response.Body)
-
-	result := make([]ResourceReference, 0)
+	doc, _ := goquery.NewDocumentFromReader(response.Body)
 	doc.Find("[class^=\"FeedScroll-feed-container-\"]").Find(".item").Each(func(_ int, s *goquery.Selection) {
 		a := s.Find("a")
 		p := s.Find("p")
@@ -48,14 +46,24 @@ func main() {
 			})
 		}
 	})
+
 	y, m, d := time.Now().Date()
 	f, _ := os.Create(fmt.Sprintf("%d-%s-%d-reuters.csv", y, m.String(), d))
-	defer f.Close()
-	c := csv.NewWriter(f)
-	c.Write([]string{"link", "title", "preview"})
 
+	// close csv file on main() end
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			fmt.Println("an error occurred when closing file. Result may be invalid")
+		}
+	}(f)
+	c := csv.NewWriter(f)
+
+	c.Write([]string{"link", "title", "preview"})
 	for _, r := range result {
 		c.Write([]string{r.href.String(), r.title, r.preview})
 	}
+
+	// Ensure full output has flushed before deferred close
 	c.Flush()
 }
